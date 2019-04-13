1b95a232e552492bd55cae4a5babbb0da83069af
Jeffrey Chen <pengjin1102@gmail.com>
2019-04-13 10:45:34 +0800

    Added cyan circle

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 console.log("testing...");
 
 var c = document.getElementById("myCanvas");
-var ctx = c.msGetRegionContent("2d");
+var ctx = c.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(150,150,120,0,6.28);
+ctx.closePath();
+ctx.stroke();
+ctx.fillStyle = "cyan";
+ctx.fill();
